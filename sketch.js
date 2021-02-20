83e7eeee792a22d1de74a9aa9244e300d06950c5
johannsofke <79168826+johannsofke@users.noreply.github.com>
2021-02-20 11:35:22 +0100

    Implemente click on hover

--- sketch.js
+++ sketch.js
@@ -1,22 +1,48 @@
 /* eslint-disable no-undef, no-unused-vars */
+let x = 200;
+let y = 200;
+let start = 0;
+let stop = 0;
+let isClick = false;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  // Put setup code here
+  createCanvas(400, 400);
+  rectMode(CENTER);
+  angleMode(DEGREES);
 }
 
 function draw() {
-  // Put drawings here
-  fill(234, 31, 81);
-  noStroke();
-  rect(50, 50, 250, 250);
-  fill(255);
-  textStyle(BOLD);
-  textSize(140);
-  text("p5*", 60, 250);
-}
+  background("white");
 
-// This Redraws the Canvas when resized
-windowResized = function() {
-  resizeCanvas(windowWidth, windowHeight);
-};
+  let isHover = mouseX > 175 && mouseX < 225 && mouseY > 175 && mouseY < 225;
+
+  if (isClick === true) {
+    fill("black");
+  } else {
+    fill("red");
+  }
+  square(x, y, 50);
+
+  if (isHover && !isClick) {
+    push();
+    stroke("green");
+    noFill();
+    circle(200, 200, 100);
+    strokeWeight(5);
+    stop = stop + 10;
+    arc(x, y, 100, 100, start - 90, stop - 90);
+
+    if (stop >= 360) {
+      isClick = true;
+      stop = 0;
+    } else {
+      isClick = false;
+    }
+    pop();
+  }
+
+  if (!isHover) {
+    isClick = false;
+    stop = 0;
+  }
+}
